6e9c40c9b4e2ba2542bafdb119382385864eb729
skirin <54919174+skirin@users.noreply.github.com>
2019-11-21 13:36:04 -0500

    Add files via upload

--- seeds.rb
+++ seeds.rb
@@ -1,28 +1,38 @@
-
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
 require 'json'
 require 'rubygems'
 
 data = File.open("the_file.json")
 read = data.read()
 
-
+@locations = Location.all
+@types = Type.all
 
 item = JSON.parse(read)
-1.upto(item.length()) do |i| 
+1.upto(item.length()) do |i|
     b = i.to_s
-    Day.create(
+    Event.create(
         title:      item[b]["title"],
-        StartDate:  item[b]["StartDate"],
-        StartTime:  item[b]["StartTime"],
-        Enddate:    item[b]["EndDate"],
-        Endtime:    item[b]["EndTime"],
-        category:   item[b]["Category"],
+        day:  item[b]["StartDate"],
+        stime:  item[b]["StartTime"],
+        enddate:    item[b]["EndDate"],
+        endtime:    item[b]["EndTime"],
+        @types.each do |type|
+          temp = Type.find(item[b]["Category"] = type.name)
+        end,
+        Type_id: temp.id,
         eventdesc:  item[b]["EventDesc"],
-        location:   item[b]["location"]
+        @locations.each do |location|
+          temp2 = Location.find(item[b]["Location"] = location.name)
+        end
+        Location_id: (temp2.id)
+
     )
-        
+
 end
-              
-
-
-
